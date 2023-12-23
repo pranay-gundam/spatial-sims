6bc36055f450ab592531d4b17abdc1524aec8985
Yanarp Madnug <pranaygundam00@gmail.com>
2023-12-22 20:36:03 -0500

    starting framework for the code

---
+++ src/Model-Constructors/params/params.py
@@ -0,0 +1,7 @@
+class Param:
+    def __init__(self, init_val, key, description, fixed = True):
+        init_value = init_val
+        value = init_val
+        key = key
+        description = description
+        fixed = fixed
---
+++ src/visualization/graphic_classes.py
@@ -0,0 +1,36 @@
+# This packages handles creating shapes and calculating distances (although distances will be encoded in a connected graph with edge weights)
+import shapely as sh
+
+class Agent:
+    def __init__(self, x_init, y_init):
+        x = x_init
+        y = y_init
+        
+
+class Household(Agent):
+    def __init__(self):
+        pass
+
+class Firm(Agent):
+    def __init__(self):
+        pass
+
+class SpatialUnit:
+    def __init__(self):
+        pass
+
+class Block(SpatialUnit):
+    def __init__(self):
+        pass
+
+class Neighborhood(SpatialUnit):
+    def __init__(self):
+        pass
+
+class Borough(SpatialUnit):
+    def __init__(self):
+        pass
+
+class City(SpatialUnit):
+    def __init__(self):
+        pass
